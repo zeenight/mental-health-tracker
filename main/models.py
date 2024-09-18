389b3983ac08fdf3ae819904a44b4ee8dd2d2fbd
zeenight <zillan.lambot@gmail.com>
2024-09-18 16:03:18 +0700

    tutor 3

--- main/models.py
+++ main/models.py
@@ -2,8 +2,10 @@ from django.db import models
 import uuid
 # Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 
 class MoodEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # tambahkan baris ini
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,11 @@
 from main.views import show_main, create_mood_entry
 from django.urls import path
 from main.views import show_main, create_mood_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import register
+from main.views import login_user
+from main.views import logout_user
+
+
 
 app_name = 'main'
 
@@ -11,5 +16,9 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    #turoeial 3
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
 
